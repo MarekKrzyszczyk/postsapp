a0a952e9b5a5a0abbed4af0ef9edd9f4a39ffb9e
m.krzyszczyk <marek.krzyszczyk@wp.pl>
2021-02-22 19:46:28 +0100

    Initial commit

---
+++ src/main/java/com/marekkrzyszczyk/postsapp/PostsappApplication.java
@@ -0,0 +1,13 @@
+package com.marekkrzyszczyk.postsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostsappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostsappApplication.class, args);
+    }
+
+}
