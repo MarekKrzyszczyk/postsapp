a1e5bbe43117084d01ea4248452761913aa861cf
m.krzyszczyk <marek.krzyszczyk@wp.pl>
2021-02-22 20:06:30 +0100

    create connection to db

--- src/main/java/com/marekkrzyszczyk/postsapp/PostsApplication.java
+++ src/main/java/com/marekkrzyszczyk/postsapp/PostsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PostsappApplication {
+public class PostsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PostsappApplication.class, args);
+        SpringApplication.run(PostsApplication.class, args);
     }
 
 }
